e51c83799b774a5e4d0d3da2f46025ad5f0b6402
riquexz <161594551+riquexz@users.noreply.github.com>
2024-05-28 10:19:04 -0300

    Update script.js

--- script.js
+++ script.js
@@ -41,3 +41,13 @@ const perguntas = [
   ]
 },
 ];
+
+let atual = 0;
+let perguntaAtual;
+
+function mostraPergunta(){
+  perguntaAtual = perguntas[atual];
+  caixaPerguntas.textContent = perguntaAtual.enunciado;
+}
+
+mostraPergunta();
