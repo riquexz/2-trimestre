0d62450f7376a322007a9b382c33f07ef4869c8b
riquexz <161594551+riquexz@users.noreply.github.com>
2024-05-28 10:09:34 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,43 @@
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
+const caixaAlternativas = document.querySelector(".caixa-alternativas");
+const caixaResultado = documento.querySelector(".caixa-resultado");
+const textoResultado = document.querySelector(".texto-resultado");
 
+const perguntas = [
+  {
+    enunciado:"Quem é o melhor do mundo?"
+    alternativas:[
+  "messi",
+  "messi",
+  ]
+},
+ {
+    enunciado:"quem é mais bonito?"
+    alternativas:[
+  "neymar",
+  "cr7",
+  ]
+},
+ {
+    enunciado:"quem tem mais bola de ouro?"
+    alternativas:[
+  "messi",
+  "cr7",
+  ]
+},
+ {
+    enunciado:"quem tem copa do mundo?"
+    alternativas:[
+  "messi",
+  "cr7",
+  ]
+},
+ {
+    enunciado:"messi é the goat?"
+    alternativas:[
+  "messi",
+  "messi",
+  ]
+},
+];
